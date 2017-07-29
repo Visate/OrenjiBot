55084708c3ddf8a2b355b5cabf11004e3ec3448e
Richard Gan <richard@designedbymajor.ca>
2017-07-29 17:07:32 -0600

    Added database and unhandled rejection handling

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const Eris = require("eris");
 const chalk = require("chalk");
 const moment = require("moment");
+const sqlite3 = require("sqlite3");
 const config = require("./config");
 
 // Intiialize the client
@@ -49,8 +50,17 @@ client.log = (...msg) => console.log(chalk.green.bold(`[LOG] [${moment().format(
 client.error = (...msg) => console.log(chalk.bgRed.white.bold(`[ERR] [${moment().format("MMM DD HH:mm:ss")}]`), ...msg);
 client.warn = (...msg) => console.log(chalk.bgYellow.white.bold(`[WRN] [${moment().format("MMM DD HH:mm:ss")}]`), ...msg);
 
+// getDatabase method
+// returns the database object when called
+client.getDatabase = () => {
+  return new sqlite3.Database(config.databaseLocation);
+};
+
 // Register modules
 require("./util/loadModules")(client);
 
 // Connect the client
 client.connect();
+
+// Catching unhandled rejections from promises
+process.on("unhandledRejection", (reason) => client.error(reason));
